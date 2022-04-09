35e35b3a319e90c523b928a5b936dd5710e57f83
Levy <levyzudo@msn.com>
2022-04-09 17:31:29 -0300

    feat: project init
    base project created

---
+++ classroom/src/database/prisma/prisma.service.ts
@@ -0,0 +1,28 @@
+import { PrismaClient } from '.prisma/client';
+import {
+  INestApplication,
+  Injectable,
+  OnModuleDestroy,
+  OnModuleInit,
+} from '@nestjs/common';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    super();
+  }
+  async onModuleInit() {
+    await this.$connect();
+  }
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+  async enableShutdownHooks(app: INestApplication) {
+    this.$on('beforeExit', async () => {
+      await app.close();
+    });
+  }
+}
