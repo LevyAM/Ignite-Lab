0784bdd7f2a89dfd3f852c675122ebb1bcc43156
Levy <levyzudo@msn.com>
2022-04-25 01:09:55 -0300

    feat: finish queries and services
    finished queries and services

--- web/src/pages/app/index.tsx
+++ web/src/pages/app/index.tsx
@@ -7,7 +7,6 @@ export default function Home() {
     <div>
       <h1>Hello World</h1>
       <pre>{JSON.stringify(user, null, 2)}</pre>
-
       <a href="/api/auth/logout">Logout</a>
     </div>
   );
